df2285745bfb4e692a5efd2d64a581f00519ee6c
Avshesh savani <avsheshs@zignuts.com>
2023-04-26 12:04:09 +0530

    update-blog and navbar

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -1,13 +1,56 @@
-import React from 'react'
-import GoogleLogin from 'react-google-login'
-import { useDispatch,useSelector } from 'react-redux'
-import { selectSignedIn,setSignedIn,setUserData } from '../features/userSlice'
-import "../styling/home.css"
+import React from "react";
+import GoogleLogin from "react-google-login";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  selectSignedIn,
+  setSignedIn,
+  setUserData,
+} from "../features/userSlice";
+
+import "../styling/home.css";
 
 const Homepage = () => {
-  return (
-    <div>Homepage</div>
-  )
-}
+  const isSignedIn = useSelector(selectSignedIn);
 
-export default Homepage
+  const dispatch = useDispatch();
+  const login = (response) => {
+    console.log(response);
+    dispatch(setSignedIn(true));
+    dispatch(setUserData(response.profileObj));
+  };
+
+  return (
+    <div className="home__page" style={{ display: isSignedIn ? "none" : "" }}>
+      {!isSignedIn ? (
+        <div className="login__message">
+          <h2>📗</h2>
+          <h1>A Readers favourite place!</h1>
+          <p>
+            We provide high quality online resource for reading blogs. Just sign
+            up and start reading some quality blogs.
+          </p>
+          <GoogleLogin
+            clientId="749473976052-us1cri7bqvue0sfgest9isn7ua07bsa8.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <button
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                className="login__button"
+              >
+                Login with Google
+              </button>
+            )}
+            onSuccess={login}
+            onFailure={login}
+            isSignedIn={true }
+            cookiePolicy={"single_host_origin"}
+          />
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
+
+export default Homepage;
